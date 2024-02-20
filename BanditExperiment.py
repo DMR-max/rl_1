440de3dae2487d812b01b620fa8a841bb2ddc3e7
DMR-max <60442394+DMR-max@users.noreply.github.com>
2024-02-20 13:06:48 +0100

    nog kijken naar e-greedy

--- BanditExperiment.py
+++ BanditExperiment.py
@@ -15,6 +15,29 @@ from Helper import LearningCurvePlot, ComparisonPlot, smooth
 
 def experiment(n_actions, n_timesteps, n_repetitions, smoothing_window):
     #To Do: Write all your experiment code here
+    average_reward_list = []
+    plot = LearningCurvePlot("plot Epsilon Greedy") # For da plot
+    for j in range(n_repetitions):
+        reward_list = []
+        env = BanditEnvironment(n_actions=n_actions) # Initialize environment 
+        pi = EgreedyPolicy(n_actions=n_actions) # Initialize policy
+        # print("Mean pay-off per action: {}".format(env.means))
+        # print("Best action = {} with mean pay-off {}".format(env.best_action,env.best_average_return))
+
+        for i in range(n_timesteps):
+            a = pi.select_action(epsilon=0.25) # select action
+            r = env.act(a) # sample reward
+            pi.update(a,r) # update policy
+            reward_list.append(r)
+            # print("Test e-greedy policy with action {}, received reward {}".format(a,r))
+        average_this_rep = sum(reward_list) / len(reward_list)
+        average_reward_list.append(average_this_rep)
+
+        # print(pi.action_val) # printing final values this repetition
+    vector_reward = np.array(average_reward_list) # for curve
+    vector_reward_smoothed = smooth(vector_reward, smoothing_window)
+    plot.add_curve(vector_reward_smoothed)
+    plot.save()
     
     # Assignment 1: e-greedy
     
--- BanditPolicies.py
+++ BanditPolicies.py
@@ -14,16 +14,24 @@ class EgreedyPolicy:
 
     def __init__(self, n_actions=10):
         self.n_actions = n_actions
+        self.action_val = np.zeros(n_actions)
         # TO DO: Add own code
         pass
         
     def select_action(self, epsilon):
-        # TO DO: Add own code
-        a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
+        random_float = np.random.uniform(0,1) 
+        if random_float < epsilon:
+            # kies een random actie
+            a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
+        else:
+            # kies argmax van de lijst met actie waarden
+            a = np.argmax(self.action_val)
+        
         return a
         
     def update(self,a,r):
-        # TO DO: Add own code
+        value = (r-self.action_val[a]) / (self.n_actions + 1)
+        self.action_val[a] += value
         pass
 
 class OIPolicy:
@@ -60,13 +68,16 @@ class UCBPolicy:
     
 def test():
     n_actions = 10
-    env = BanditEnvironment(n_actions=n_actions) # Initialize environment    
-    
-    pi = EgreedyPolicy(n_actions=n_actions) # Initialize policy
-    a = pi.select_action(epsilon=0.5) # select action
-    r = env.act(a) # sample reward
-    pi.update(a,r) # update policy
-    print("Test e-greedy policy with action {}, received reward {}".format(a,r))
+    env = BanditEnvironment(n_actions=n_actions) # Initialize environment 
+    pi = EgreedyPolicy(n_actions=n_actions) # Initialize policy   
+    print("Mean pay-off per action: {}".format(env.means))
+    print("Best action = {} with mean pay-off {}".format(env.best_action,env.best_average_return))
+
+    for i in range(10):
+        a = pi.select_action(epsilon=0.5) # select action
+        r = env.act(a) # sample reward
+        pi.update(a,r) # update policy
+        print("Test e-greedy policy with action {}, received reward {}".format(a,r))
     
     pi = OIPolicy(n_actions=n_actions,initial_value=1.0) # Initialize policy
     a = pi.select_action() # select action
